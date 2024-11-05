0565fd345b064de803c04b3bbc6c042dd8fff6b7
CryptoKong28 <petals.brewers.0b@icloud.com>
2024-11-04 23:25:02 -0700

    Update and rename hello.py to hello.js

--- api/hello.js
+++ api/hello.js
@@ -2,4 +2,6 @@ def handler(request):
     return {
         "statusCode": 200,
         "body": "Hello, world! This is a test endpoint."
-    }
+module.exports = (req, res) => {
+  res.status(200).json({ message: "Hello, world! This is a test endpoint." });
+};
