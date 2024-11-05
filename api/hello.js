b6e54bab7e852b776eee11e35d43ea24acd67d56
CryptoKong28 <petals.brewers.0b@icloud.com>
2024-11-04 23:29:25 -0700

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,7 +1,3 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": "Hello, world! This is a test endpoint."
 module.exports = (req, res) => {
-  res.status(200).json({ message: "Hello, world! This is a test endpoint." });
+  res.status(200).json({ message: "Hello from Vercel!" });
 };
